52b163cdb792d7984f98e8535372628a0a2de9ba
AnsQuartz07 <anshu.dakshana16@gmail.com>
2025-04-11 23:15:35 +0530

    include cors tresspassing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const authRoutes = require('./Routes/authRoutes.js');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -32,6 +33,23 @@ app.use((req, res, next) => {
 
 // Routes Middlewares
 app.use('/api/user',authRoutes);
+
+// cors trespassing
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://ans-service.onrender.com',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 // starting the server
 app.listen(process.env.PORT, function () {
